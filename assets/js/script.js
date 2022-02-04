83fb3aa52304cbe1dae98aed9aee2a2d0334ae17
Niksharpkings <67552318+Niksharpkings@users.noreply.github.com>
2022-02-04 00:14:11 -0800

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -226,6 +226,22 @@ var saveTasks = function() {
 localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var loadTasks = function() {
+  var savedTasks = localStorage.getItem("tasks");
+  if (!savedTasks) {
+    return false;
+  }
+
+  savedTasks = JSON.parse(savedTasks); 
+  
+  //loop through savedTasks array
+  for (var i = 0; i < savedTasks.length; i++) {
+    //pass each task onject into the 'createTaskEl()' function
+    createTaskEl(savedTasks[i]);
+  }
+
+}
+
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
